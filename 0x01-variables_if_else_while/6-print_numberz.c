24d56a8fc48e250a0520a74044b0a0c96d221b57
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-09-18 00:39:32 +0000

    number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main- all letters of the alphabet
+ *
+ * Return: 0 (program ran)
+ */
+int main(void)
+{
+int number;
+while(number < 10)
+{
+putchar(number);
+number++;
+}
+putchar('\n');
+return (0);
+}
