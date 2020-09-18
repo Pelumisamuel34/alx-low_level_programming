64c76caa8ed4c0d763adf5d3ec04c7c68d0faad6
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-09-18 01:17:40 +0000

    char removed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 int main(void)
 {
 int number;
 int comma = ',';
-char space = ' ';
 while (number < 10)
 {
 putchar(number++);
 putchar(comma);
-putchar(space);
+putchar(' ');
 }
 putchar('\n');
 return (0);
