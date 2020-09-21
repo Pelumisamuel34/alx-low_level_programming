19f156df2ee751ac01440f8c839d00d697fcb69c
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-09-21 16:58:34 +0000

    created header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+
+void print_alphabet(void)
+{
+char i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+
+}
+
+int main(void)
+{
+void print_alphabet(void);
+return (0);
+}
