e4a6f48883d88c1f61a89f8ef6420e8ec1ce2474
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-09-21 15:45:04 +0000

    while loop used

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
 int main(void)
 {
-printf("Holberton");
+char school[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+putchar(school[i]);
+i++;
+}
+putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a deepdive into the c programing
