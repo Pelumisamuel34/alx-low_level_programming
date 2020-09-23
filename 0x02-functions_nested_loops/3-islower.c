4649767fb5f2697c8710a6496f69b128adfc52ae
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-09-23 20:52:09 +0000

    Question 3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*create a function module, not a program. Very important
+*
+*
+*/
+
+int _islower()
+{
+int c;
+
+if (97 <= c <= 122)
+{ return (1);
+}
+else 
+{ return 0;
+}
+}
