ebf85fc05db42c95b170cb8d464e161eb7403a50
Pelumi <oluwapelumi.d.samuel@gmail.com>
2020-10-02 20:50:50 +0000

    copied

---
+++ 0x06-printers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+wap_int(int *a, int *b)
+{
+	*a = *b + *a;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+/**
+ * reverse_array - reverses the contents of an array of integers
+ * @a: array of integers
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int counter = 0;
+
+	while (counter < n / 2)
+	{
+		/*if you have an  array of n elements the last element is at n - 1*/
+		swap_int(a + counter, a + (n - counter - 1));
+		counter++;
+	}
+
+
+}
---
+++ 0x06-printers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is the more pointers and concatonations project
+
